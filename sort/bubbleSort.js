daeb2195dffcab0fdeb24ee90787eb4c06465849
aurpo1 <aurpo1@naver.com>
2022-06-09 23:11:37 +0900

    feat: insert sorting and bubble sorting in javascript

---
+++ sort/bubbleSort.js
@@ -0,0 +1,13 @@
+function bubbleSort(arr) {
+  for(let i=n-1; i>0; i--) { 
+    for(let j=0; j<=i; j++) {
+      if (arr[j] > arr[j+1]) { // 왼쪽에 위치한 원소가 더 작다 -> 있어야할 위치가 아니다.
+        // swap
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+  return arr;
+}
---
+++ sort/insertSort.js
@@ -0,0 +1,16 @@
+function insertSort(arr) {
+  for(let i=1; i<input.length; i++) {
+    let temp = arr[i];
+
+    let prev = i-1;
+
+    while (prev >= 0 && arr[prev] > temp) { // temp가 정렬된 원소보다 작으면 정렬된 원소를 뒤로 옮긴다.
+      arr[prev+1] = arr[prev];
+      prev--;
+    }
+
+    // temp와 앞의 원소들이 다 정렬되었으니 temp는 temp의 자리에 넣는다.
+    arr[prev+1] = temp;
+  }
+  return arr;
+}
