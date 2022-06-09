f709e4594a77a2fac24f8af97d97f71d1703b579
aurpo1 <aurpo1@naver.com>
2022-06-10 01:01:05 +0900

    feat: create programmers folder and solve exam in js

---
+++ programmers/exam.js
@@ -0,0 +1,28 @@
+function solution(answers) {
+  var answer = [];
+  let result = [0, 0, 0];
+  
+  const p1 = [1, 2, 3, 4, 5];
+  const p2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  const p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+  for (let i=0; i<answers.length; i++) {
+    if (answers[i] === p1[i%5]) {
+      result[0]++;
+    }
+    if (answers[i] === p2[i%8]) {
+      result[1]++;
+    }
+    if (answers[i] === p3[i%10]) {
+      result[2]++;
+    }
+  }
+
+  for (let i=0; i<3; i++) {
+    if (result[i] === Math.max(...result)) {
+      answer.push(i+1);
+    }
+  }
+
+  return answer;
+}
