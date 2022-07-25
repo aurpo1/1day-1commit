dabe59e2d4d4b3137bd2e724775b2471b29ae96e
aurpo1 <aurpo1@naver.com>
2022-07-26 01:19:37 +0900

    fix: fix 덩치 problem

--- bruteforcing/big.js
+++ bruteforcing/big.js
@@ -9,12 +9,23 @@ let result = Array(n).fill(1, 0);
 
 for (let i = 0; i < people.length; i++) {
   for (let j = 0; j < people.length; j++) {
-    if (people[i][0] < people[j][0] && people[i][1] < people[j][1]) {
+    if (
+      Number(people[i][0]) < Number(people[j][0]) &&
+      Number(people[i][1]) < Number(people[j][1])
+    ) {
       result[i]++;
     }
   }
 }
 
+let print = "";
 for (let i = 0; i < result.length; i++) {
-  process.stdout.write(result[i] + " ");
+  print += String(result[i]) + " ";
 }
+
+console.log(print);
+
+// 또 다른 출력 방법
+// for (let i = 0; i < result.length; i++) {
+//   process.stdout.write(result[i] + " ");
+// }
