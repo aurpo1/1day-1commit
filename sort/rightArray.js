0c3b6f935d42e655154f82289af66da5cf69e005
aurpo1 <aurpo1@naver.com>
2022-07-28 23:56:31 +0900

    feat: solve rigth Array in js

---
+++ sort/rightArray.js
@@ -0,0 +1,41 @@
+// 올바른 배열
+
+const fs = require("fs");
+const input = fs
+  .readFileSync("input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+const n = input.shift();
+input.sort((a, b) => a - b);
+
+let count = 0;
+
+for (let i = 0; i < n; i++) {
+  let tempCount = 0;
+  // switch (input[i] - input[i - 1]) {
+  //   case 1:
+  //     tempCount++;
+  //     if (tempCount > count) {
+  //       count = tempCount;
+  //     }
+  //     break;
+  //   case 0:
+  //     break;
+  //   default:
+  //     tempCount = 0;
+  //     break;
+  // }
+  for (let j = i + 1; j < i + 5; j++) {
+    if (input[j] - input[i] < 5) {
+      tempCount++;
+    }
+    if (tempCount > count) {
+      count = tempCount;
+    }
+  }
+}
+
+console.log(4 - count);
