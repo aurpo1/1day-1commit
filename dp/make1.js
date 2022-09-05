06eb27e73202352da32afaf772f9874e92966af4
aurpo1 <aurpo1@naver.com>
2022-09-06 00:43:18 +0900

    fix

--- dp/make1.js
+++ dp/make1.js
@@ -8,11 +8,17 @@ let dp = [];
 dp[1] = 0;
 dp[2] = 1;
 dp[3] = 1;
+dp[4] = 2;
+dp[5] = 3;
+dp[6] = 2;
 
-for (let i = 4; i <= n; i++) {
-  dp[i] = dp[i - 1] + 1;
-
-  if (i % 2 === 0) dp[i] = Math.min(dp[i - 1], dp[i / 2]) + 1;
-  if (i % 3 === 0) dp[i] = Math.min(dp[i - 1], dp[i / 3]) + 1;
+for (let i = 7; i <= n; i++) {
+  if (i % 6 === 0) {
+    dp[i] = Math.min(dp[i - 1], dp[i / 2], dp[i / 3]) + 1;
+  } else if (i % 2 === 0) {
+    dp[i] = Math.min(dp[i - 1], dp[i / 2]) + 1;
+  } else if (i % 3 === 0) {
+    dp[i] = Math.min(dp[i - 1], dp[i / 3]) + 1;
+  } else dp[i] = dp[i - 1] + 1;
 }
 console.log(dp[n]);
