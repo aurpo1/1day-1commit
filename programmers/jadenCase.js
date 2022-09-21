dfeee60a6d86f268a02dd44138a800d0f5f27112
aurpo1 <aurpo1@naver.com>
2022-09-22 00:37:57 +0900

    solve `jadencase 문자열 만들기` in js

---
+++ programmers/jadenCase.js
@@ -0,0 +1,11 @@
+// 프로그래머스 JadenCase 문자열 만들기
+
+function solution(s) {
+  var answer = "";
+
+  for (let i = 0; i < s.length; i++) {
+    if (i === 0 || s[i - 1] === " ") answer += s[i].toUpperCase();
+    else answer += s[i].toLowerCase();
+  }
+  return answer;
+}
