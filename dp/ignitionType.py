d97c51aa2bff53181b4a265e24128ceedb62226a
aurpo1 <aurpo1@naver.com>
2022-10-13 15:24:03 +0900

    solve `점화식` in py

---
+++ dp/ignitionType.py
@@ -0,0 +1,15 @@
+# 백준 13699번 점화식
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] * (n+1)
+dp[0] = 1
+dp[1] = 1
+
+for i in range(2, n+1):
+  for j in range(i):
+    dp[i] += dp[j] * dp[i-j-1]
+
+print(dp[n])
